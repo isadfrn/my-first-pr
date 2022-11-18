f66db2a3768f2d968a454568c27919070a46c594
isadfrn <isadfrn@gmail.com>
2022-11-18 20:36:11 -0300

    feat: add student card

---
+++ src/components/InfoCard/index.jsx
@@ -0,0 +1,31 @@
+import linkedinIcon from '../../assets/linkedin.svg';
+import githubIcon from '../../assets/github.svg';
+
+import './style.scss';
+
+export function InfoCard({
+  profileImageUrl,
+  selfDescription,
+  name,
+  title,
+  linkedinUrl,
+  githubUrl,
+}) {
+  return (
+    <div className="card">
+      <img className="profileImage" src={profileImageUrl} alt={selfDescription} />
+      <div className="texts">
+        <h2>{name}</h2>
+        <p>{title}</p>
+        <div className="icons">
+          <a href={linkedinUrl}>
+            <img className="icon" src={linkedinIcon} alt={`${name} LinkedIn URL`} />
+          </a>
+          <a href={githubUrl}>
+            <img className="icon" src={githubIcon} alt={`${name} GitHub URL`} />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
